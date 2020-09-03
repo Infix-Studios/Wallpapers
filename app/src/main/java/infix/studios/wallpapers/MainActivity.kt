48450938f7d17e8fd304cb374cbfc6ebf9bcf683
JoseMarrima <benderspor@gmail.com>
2020-09-03 19:22:03 +0200

    working

--- app/src/main/java/infix/studios/wallpapers/MainActivity.kt
+++ app/src/main/java/infix/studios/wallpapers/MainActivity.kt
@@ -2,13 +2,18 @@ package infix.studios.wallpapers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +23,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupViews() {
         // Finding the Navigation Controller
-        var navController = findNavController(R.id.fragNavHost)
+        var navHostFragment = supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
+        navController = navHostFragment.navController
 
-        // Setting Navigation Controller with the BottomNavigationView
-        bottomNavView.setupWithNavController(navController)
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
+
 
         // Setting Up ActionBar with Navigation Controller
         // Pass the IDs of top-level destinations in AppBarConfiguration
-        var appBarConfiguration = AppBarConfiguration(
+        val appBarConfiguration = AppBarConfiguration(
             topLevelDestinationIds = setOf (
                 R.id.homeFragment,
                 R.id.categoriesFragment,
@@ -35,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         )
 
         // Setting Up ActionBar with Navigation Controller
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+
     }
 }
